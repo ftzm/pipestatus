c0bd6c7dcc28ea6d9e93a3c690442d6a05381286
ftzm <matthew@fitzsimmons.org>
2019-07-03 09:35:08 +0200

    add .dir-locals.el for dante

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((haskell-mode . ((dante-repl-command-line . ("nix-shell" "--attr" "pipestatus.env" "/home/matt/dev/pipestatus/release.nix" "--run" "cabal repl --builddir=dist/dante")))))
+((haskell-mode . ((dante-repl-command-line . ("nix-shell" "--attr"
+					      "pipestatus.env" (concat
+					      (expand-file-name dante-project-root) "release.nix") "--run" "cabal new-repl --builddir=dist/dante")))))
