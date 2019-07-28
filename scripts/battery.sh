d1a8736905d7af51bccc4ed50422987ceb09f5e9
ftzm <matthew@fitzsimmons.io>
2019-07-28 16:28:50 +0200

    add missing scripts directory

---
+++ scripts/battery.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+status=$(</sys/class/power_supply/BAT0/status)
+capacity=$(</sys/class/power_supply/BAT0/capacity)
+energy_now=$(</sys/class/power_supply/BAT0/energy_now) # Remaining numerator
+power_now=$(</sys/class/power_supply/BAT0/power_now) # Remaining denominator
+
+echo "b;$status $capacity $energy_now $power_now" > /tmp/statuspipe.fifo
